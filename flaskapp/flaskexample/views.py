32205943ffb160e9621647029e651064b6975bac
Katrina Kutchko <kutchko@email.unc.edu>
2017-06-15 00:10:01 -0400

    Added Flask app!

---
+++ flaskapp/flaskexample/views.py
@@ -0,0 +1,106 @@
+from flask import render_template
+from flask import request
+from flaskexample import app
+import pymysql.cursors
+import pandas as pd
+
+user = 'kkutchko'
+passwd = 'password'
+database = 'metro_areas'
+
+connection = pymysql.connect(host='localhost',
+        user=user,
+        password=passwd,
+        db=database,
+        cursorclass=pymysql.cursors.DictCursor)
+
+
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/input')
+def cities_input():
+    with connection.cursor() as cursor:
+        cities_query = "select distinct(CBSA) from metro_data";
+        cursor.execute(cities_query)
+        rows = cursor.fetchall()
+        
+        cities = [r['CBSA'] for r in rows]
+
+        return render_template('input.html', cities = cities)
+
+@app.route('/output')
+def cities_output():
+    recommended_city = ''
+    city = request.args.get('city')
+
+    t = request.args.get('good_for_tech')
+    c = request.args.get('cheaper')
+    d = request.args.get('diversity')
+
+    query2 = "select CBSA from metro_data" 
+    subqueries = []
+    if c or d:
+        query1 = "select * from metro_data where CBSA = %s"
+        row = []
+        with connection.cursor() as cursor:
+            cursor.execute(query1, (city) )
+            row = cursor.fetchone()
+
+            if c:
+                subqueries.append("MedianRent < %s" % row['MedianRent'])
+            if d:
+                subqueries.append("DiversityIndex > %s" % row['DiversityIndex'])
+    if t:
+        subqueries.append("GoodForTech = 'Yes'")
+    if subqueries:
+        query2 += " where "
+        query2 += " and ".join(subqueries)
+    
+    query3 = "select * from city_distance where CBSA1 = %s and CBSA2 in (" +\
+        query2 + ") order by distance limit 1"
+    with connection.cursor() as cursor:
+        cursor.execute(query3, (city) )
+        if not cursor.rowcount:
+            recommended_city = 'No cities found!'
+        else:
+            row = cursor.fetchone()
+            recommended_city = row['CBSA2']
+
+
+    return render_template('output.html',
+        reccity = recommended_city)
+
+
+
+@app.route('/db')
+def birth_page():
+    sql_query = """
+                select * from city_distance where CBSA1 like '%St. George, UT%';
+                """
+    query_results = pd.read_sql_query(sql_query, connection)
+    results = ""
+    for i in range(0, 10):
+        results += query_results.iloc[i]['CBSA2']
+        results += "<br>"
+    return results
+
+@app.route('/db_fancy')
+def birth_page_fancy():
+    sql_query = """
+                select * from city_distance where CBSA1 like '%St. George, UT%';
+                """
+    query_results = pd.read_sql_query(sql_query, connection)
+    res = []
+    for i in range(0, query_results.shape[0]):
+        res.append(dict(index = i,
+                        attendant = query_results.iloc[i]['CBSA2'],
+                        birth_month = query_results.iloc[i]['distance']))
+    return render_template('starter-template.html', births = res)
+
+
+
+
+
+
